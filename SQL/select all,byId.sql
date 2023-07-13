c31e998c580850f2c3e146c29f53b8124e7ed82a
rishabh2298 <rishabhmishra2298@gmail.com>
2023-07-13 22:13:25 +0530

    select by id

---
+++ SQL/select all,byId.sql
@@ -0,0 +1,17 @@
+
+Select
+*
+from city;
+
+/*
+Query all columns for a city in CITY with the ID 1661.
+The CITY table is described as follows:
+
+Link : https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+
+*/
+
+select
+*
+from city
+where id=1661;
