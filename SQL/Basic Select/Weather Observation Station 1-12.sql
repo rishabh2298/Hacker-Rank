ebecdce184b315dea0cb34de6d78ba4d7b35086f
rishabh2298 <rishabhmishra2298@gmail.com>
2023-07-22 22:35:08 +0530

    Good question (two diff quesries)

--- SQL/Basic Select/Weather Observation Station 1-12.sql
+++ SQL/Basic Select/Weather Observation Station 1-12.sql
@@ -30,3 +30,26 @@ Link : https://www.hackerrank.com/challenges/weather-observation-station-4/probl
 
 SELECT count(city)-count(DISTINCT city)
 FROM station;
+
+/*
+5. Weather Observation Station 5
+
+Query the two cities in STATION with the shortest and longest CITY names, as well as 
+their respective lengths (i.e.: number of characters in the name). If there is more 
+than one smallest or largest city, choose the one that comes first when ordered 
+alphabetically.
+
+ GOOD Ques
+
+ Link : https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+*/
+
+SELECT city, length(city) AS len
+FROM station
+ORDER BY len asc, city
+LIMIT 1;
+
+SELECT city, length(city) AS len
+FROM station
+ORDER BY len desc, city desc
+LIMIT 1;
