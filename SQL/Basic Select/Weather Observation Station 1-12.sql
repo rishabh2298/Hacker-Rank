2fe3edf332f461dafbc3bf819dd95b8742ee79cb
rishabh2298 <rishabhmishra2298@gmail.com>
2023-07-22 23:46:16 +0530

    Left() example

--- SQL/Basic Select/Weather Observation Station 1-12.sql
+++ SQL/Basic Select/Weather Observation Station 1-12.sql
@@ -53,3 +53,17 @@ SELECT city, length(city) AS len
 FROM station
 ORDER BY len desc, city desc
 LIMIT 1;
+
+
+/*
+ 6. Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) 
+    from STATION. 
+    Your result cannot contain duplicates.
+
+    Link : https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+*/
+
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city,1) IN ('a','e','i','o','u');
