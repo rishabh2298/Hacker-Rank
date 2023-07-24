507a3aaba62c41995969562b4300ca1b944aa701
rishabh2298 <rishabhmishra2298@gmail.com>
2023-07-24 23:27:48 +0530

    LEFT() AND RIGHT(), NOT IN

--- SQL/Basic Select/Weather Observation Station 1-12.sql
+++ SQL/Basic Select/Weather Observation Station 1-12.sql
@@ -136,3 +136,17 @@ WHERE RIGHT(city,1) NOT IN ('a','e','i','o','u');
 SELECT DISTINCT city
 FROM station
 WHERE LEFT(city,1) NOT IN ('a','e','i','o','u') OR RIGHT(city,1) NOT IN ('a','e','i','o','u');
+
+
+
+/*
+ 12.  Weather Observation Station 12
+      Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+      Your result cannot contain duplicates.
+
+    Link : https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true
+*/
+
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city,1) NOT IN ('a','e','i','o','u') AND RIGHT(city,1) NOT IN ('a','e','i','o','u');
